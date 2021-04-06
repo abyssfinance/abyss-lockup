25f2297ec8809862f34e326d5d305aba81a0c09b
Sephiroth <sephiroth@abyss.finance>
2021-04-06 19:25:43 +0300

    Openzeppelin 4.0.0

--- contracts/AbyssSafeBase.sol
+++ contracts/AbyssSafeBase.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "../contracts/interfaces/IAbyssLockup.sol";
 
